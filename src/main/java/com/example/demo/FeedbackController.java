af720f79f7290c0378589e4275592ae225b70c42
Victor Phimphachanh <phimphachanh.momo.victor@gmail.com>
2019-06-05 12:50:05 -0400

    email project

---
+++ src/main/java/com/example/demo/FeedbackController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.ValidationException;
+
+@RestController
+@RequestMapping("/feedback")
+public class FeedbackController {
+    private EmailCfg emailCfg;
+
+    public FeedbackController(EmailCfg emailCfg) {
+        this.emailCfg = emailCfg;
+    }
+
+    @PostMapping
+    public void sendFeedback(@RequestBody Feedback feedback, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new ValidationException("Feedback is not valid");
+        }
+
+        //Create a mail sender
+        JavaMailSender mailSender = new JavaMailSenderImpl();
+        mailSender.
+    }
+
+}
